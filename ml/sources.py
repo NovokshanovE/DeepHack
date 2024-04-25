c98a8ded4e272be0076192d443cec693c91424f0
Owling797 <zavadskayalus@gmail.com>
2024-04-26 02:50:42 +0300

    added title, links, annotation to result

--- ml/sources.py
+++ ml/sources.py
@@ -11,24 +11,46 @@ from ..evgeny.parser import parser_links, parser
 
 credentials = 'Yjg4MTQzMmUtNDAwMS00NDk0LThjOGUtNmU5ZWQ2YzQ4NDQ2OmQ4MWMxZGZiLTFmNGYtNDk5NS05OGQzLTBiMzYyYWJmNjk3OA=='
 llm = GigaChat(credentials=credentials, verify_ssl_certs=False, scope="GIGACHAT_API_CORP")
+embeddings = GigaChatEmbeddings(credentials=credentials, verify_ssl_certs=False, scope="GIGACHAT_API_CORP")
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=50,
+)
 
-# название статьи
-# ссыль
-# краткое описание
+
 def get_sources_links(llm, prompt = "математика в машинном обучении"):
     prompt = translate_to_eng(prompt, llm)
     links = parser_links(prompt)
-    docs = [parser(i) for i in links]
+    docs = [parser(i) for i in links] # {"title": title, "text": text, "links": links}
     
     filenames = []
     for i, doc in enumerate(docs):
-        filenames.append(encode_file(doc, links[i]))
+        filenames.append(encode_file(doc["text"], links[i])) # text == link
     db = sources_initialization(filenames)
     
-    docs = db.similarity_search(prompt, k=20)
-    return [doc[i].metadata['source'] for i in docs]
+    result_docs = db.similarity_search(prompt, k=20)
+    
+    result = []
+    for doc in result_docs:
+        link = decode_link_from_file(doc[i].metadata['source'])#[len("docs/"):-len(".txt")]
+        doc_index = find_doc_by_link(link, links)
+        title = docs[doc_index]['title']
+        outer_sources = docs[doc_index]['links']
+        annotation = docs[doc_index]['text']
+        result.append({
+            "title": title,
+            "links": outer_sources,
+            "annotation": annotation
+        })
+    return result
     
     
+def encode_name(name):
+    name = name.replace("/", "(")
+    name = name.replace(".", "_")
+    name = name.replace(":", ")")
+    return name 
+
 
 def encode_file(doc, link):
     name = link
@@ -38,13 +60,18 @@ def encode_file(doc, link):
     with open(f"docs/{name}.txt", "w", encoding="utf-8") as file:
         file.write(doc)
     return f"docs/{name}.txt"
+
     
 def decode_link_from_file(file_name):
-    name = file_name[:-4]
+    name = file_name[len("docs/"):-len(".txt")]
     name = name.replace("(", "/")
     name = name.replace("_", ".")
     name = name.replace(")", ":")
     return name
+
+
+def find_doc_by_link(link, links):
+    return links.index(link)
 
 
 def detect_language(llm, text):
@@ -63,21 +90,12 @@ def translate_to_eng(llm, text):
     
 
 def sources_initialization(filenames: list):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400,
-        chunk_overlap=50,
-    )
-    
-    
     documents=[]
     for filename in filenames:
         loader = TextLoader(filename)
         document = loader.load()
         document = text_splitter.split_documents(document)
         documents.append(document)
-    #print(f"Total documents: {len(documents)}")
-
-    embeddings = GigaChatEmbeddings(credentials=credentials, verify_ssl_certs=False, scope="GIGACHAT_API_CORP")
 
     db = Chroma.from_documents(
         documents,
