8401a420b6ab453c9fc4c316487a23085701dd47
NovokshanovE <evgeny.novokshanov@gmail.com>
2024-04-25 18:45:40 +0300

    fix bugs and test example

---
+++ evgeny/main.py
@@ -0,0 +1,21 @@
+"""Пример работы с чатом через gigachain"""
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+# Авторизация в сервисе GigaChat
+chat = GigaChat(credentials='Yjg4MTQzMmUtNDAwMS00NDk0LThjOGUtNmU5ZWQ2YzQ4NDQ2OmQ4MWMxZGZiLTFmNGYtNDk5NS05OGQzLTBiMzYyYWJmNjk3OA==', verify_ssl_certs=False,scope='GIGACHAT_API_CORP')
+
+messages = [
+    SystemMessage(
+        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+    )
+]
+
+while(True):
+    # Ввод пользователя
+    user_input = input("User: ")
+    messages.append(HumanMessage(content=user_input))
+    res = chat(messages)
+    messages.append(res)
+    # Ответ сервиса
+    print("Bot: ", res.content)
